867205bf3583390a271f752891632c2fb20f4e67
Sinojapien <1155126460@link.cuhk.edu.hk>
2023-09-26 15:48:54 -0300

    fix leaflet map marker

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -2,6 +2,9 @@
     import { onMounted, defineProps, toRef } from "vue";
 
     import "leaflet/dist/leaflet.css";
+    import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+    import iconUrl from "leaflet/dist/images/marker-icon.png";
+    import shadowUrl from "leaflet/dist/images/marker-shadow.png";
     import Leaflet from "leaflet";
 
     import { useMapMarkersStore } from "../stores/mapMarkers";
@@ -13,6 +16,10 @@
 
     onMounted(() => {
         // Map Initialization
+
+        // Fix leaflet marker on production
+        Leaflet.Icon.Default.mergeOptions({ iconRetinaUrl, iconUrl, shadowUrl });
+
         const map = Leaflet.map("mapContainer").setView(center.value, 13);
         Leaflet.tileLayer(import.meta.env.VITE_MAP_TILE_URL, {
             maxZoom: 19,
